ba7a39219e966c613aa1cb7046cc2483dbf9badb
Lautaro Di Sanza <lautarod@gmail.com>
2024-08-27 11:26:25 -0300

    Update info Schemas

--- backend/src/certificacion/dto/certificacion.dto.ts
+++ backend/src/certificacion/dto/certificacion.dto.ts
@@ -1,11 +1,17 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateCertificacionDTO {
+  @ApiProperty({ example: '66cdb0b728d61400860a348b' })
   readonly id_curso: string;
+  @ApiProperty({ example: '66cdb0b728d61400860a348b' })
   readonly id_desafio: string;
+  @ApiProperty({ example: '66cdb0b728d61400860a348b' })
   readonly id_usuario: string;
+  @ApiProperty({ example: true })
   readonly estado: boolean;
+  @ApiProperty({ example: '2024-08-27' })
   readonly fecha_certificacion: Date;
+  @ApiProperty({ example: '66cdb0b728d61400860a348b' })
   readonly admin_certificador: string;
   readonly createdat: Date;
 }
